6727784f96aef8041c13851ff6926a7dd736d4dd
James Colannino <james@colannino.dev>
2024-10-04 21:11:54 -0700

    fix the fillets

--- stirer/thermometer-probe-mount.py
+++ stirer/thermometer-probe-mount.py
@@ -1,6 +1,7 @@
 length = 34.54
 width = 15
 height = 23
+probeRadius = 1.7
 filletRadius = 3.2
 thickness = filletRadius * 2 + 0.1 # The thickness must be greater than filletRadius * 2
 
@@ -18,4 +19,9 @@ r = cq.Workplane("front").hLine(length)\
 
 result = r.mirrorY().extrude(width)  # mirror the geometry and extrude
 
-result = result.edges("|Z").fillet(filletRadius)
+# This adds the rounded inner edges for the bracket to sit comfortably
+# on top of the stirer
+result = result.edges("|Z and <<Y[-2]").fillet(filletRadius)
+
+# This just rounds the top to make it smoother to the touch
+result = result.edges("|Z and <Y").fillet(1)
