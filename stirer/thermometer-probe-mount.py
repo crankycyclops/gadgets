6dc12bb5c8c44507e78d08579f0bb406ddfb46bd
James Colannino <james@colannino.dev>
2024-10-06 14:13:25 -0700

    made corrections to the temperature probe mount model

--- stirer/thermometer-probe-mount.py
+++ stirer/thermometer-probe-mount.py
@@ -1,14 +1,17 @@
-length = 34.54
+innerLength = 34.54
 width = 15
 height = 23
-probeRadius = 1.7
+probeRadius = 1.8
 filletRadius = 3.2
 
 probeHousingHeight = height / 2.5
 thickness = filletRadius * 2 + 0.1 # The thickness must be greater than filletRadius * 2
 
+# This is to ensure that the length of the inside is exactly the value above
+outerLength = innerLength + 2 * thickness
+
 # Draws half the C-shaped bracket
-result = cq.Workplane("front").hLine(length / 2)\
+result = cq.Workplane("front").hLine(outerLength / 2)\
     .vLine(height)\
     .hLine(-thickness)\
     .vLine(-height + thickness)\
@@ -27,16 +30,16 @@ result = result.edges("|Z and <Y").fillet(1)
 # Subtract a smaller cylinder to create the hole for the probe.
 # This should have just enough clearance for a tight fit.
 innerCylinder = cq.Workplane("XZ").cylinder(probeHousingHeight, probeRadius)\
-    .translate((length / 2 + (probeRadius * 2) - (0.5 * probeRadius), height / 2, width / 2))
+    .translate((outerLength / 2 + (probeRadius * 2) - (0.5 * probeRadius), height / 2, width / 2))
 
 # Add the cylindrical housing for the probe. Use probeRadius
 # to size the cylinder.
 outerCylinder = cq.Workplane("XZ").cylinder(probeHousingHeight, probeRadius * 2)\
-    .translate((length / 2 + (probeRadius * 2) - (0.5 * probeRadius), height / 2, width / 2))\
+    .translate((outerLength / 2 + (probeRadius * 2) - (0.5 * probeRadius), height / 2, width / 2))\
     .cut(innerCylinder)
 
 # This is the tighter inner width that the probe pushes into
-snapInVLineInner = probeRadius * 0.8
+snapInVLineInner = probeRadius * 0.9
 
 # This is the outer width of the snap in for the probe
 snapInVLineOuter = probeRadius * 1.2
@@ -54,7 +57,7 @@ snapIn = cq.Workplane("XZ").moveTo(-probeRadius / 2, -probeRadius / 2)\
     .vLine(-snapInVLineInner)\
     .close()\
     .extrude(probeHousingHeight)\
-    .translate((length / 2 + (probeRadius * 2) - (0.5 * probeRadius), height / 2, width / 2))\
+    .translate((outerLength / 2 + (probeRadius * 2) - (0.5 * probeRadius), height / 2, width / 2))\
     .translate((probeRadius * 1.5 - 0.2, probeHousingHeight / 2, 0))
 
 outerCylinder = outerCylinder.cut(snapIn)
